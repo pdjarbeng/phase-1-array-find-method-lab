0638558251571e49eba3a58b0116a77b424c08e1
priscilla <pdjarbeng@outlook.com>
2021-10-15 21:47:46 -0400

    commit tree 0c5b038c3a41da5fc5efcf6a1a2a6e5a8adeae64

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+// const record = [
+//     { year: "2018", result: "W"},
+// ]
+
+function superbowlWin(array) {
+    const results = array.find(key => key.result ==="W") 
+    return results ? results.year :undefined
+}
+
